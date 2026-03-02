88bdd93766757f446bff252f7af707b9849706e9
jamienguyen-rgb <jamienguyen@uchicago.edu>
2026-03-02 17:54:37 -0600

    Update 2_Explore.py

--- pages/2_Explore.py
+++ pages/2_Explore.py
@@ -1,14 +1,15 @@
 import streamlit as st
-from utils.io import load_data
+from utils.io import (load_points, load_wins)
 from charts.charts import dashboard
 
 st.set_page_config(page_title="Explore", layout="wide")
-df = load_data()
+points_df = load_points()
+win_df = load_wins()
 
 st.title("Interactive Exploratory View")
 st.write("Use interaction to validate and extend the story—select one team, then see their performance over each season.")
 
-st.altair_chart(dashboard(df), use_container_width=True)
+st.altair_chart(dashboard(points_df, wins_df), use_container_width=True)
 
 st.markdown("**Guided prompts:**")
 st.write("- Select one team (e.g., `Arsenal`, `Man United`)—how does their performance vary across seasons?")
